14ff79280a9539c05219a99ec89f977bf8af1327
Chris George <cgeorge110@gmail.com>
2014-02-09 16:13:42 -0800

    Amazon SES Update

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,1 +1,7 @@
-
+ActionMailer::Base.smtp_settings = {
+   :address => "email-smtp.us-west-2.amazonaws.com",
+    :user_name => ENV["SES_USERNAME"],
+    :password => ENV["SES_PASSWORD"],
+    :authentication => :login,
+    :enable_starttls_auto => true
+}
