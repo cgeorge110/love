ef78c181df923f92fa2306986cbbd32a4d6b3c06
Chris George <cgeorge110@gmail.com>
2014-02-09 20:03:00 -0800

    Push Image Links

--- app/mailers/love_mail.rb
+++ app/mailers/love_mail.rb
@@ -3,8 +3,6 @@ class LoveMail < ActionMailer::Base
 
   def love_note(email)
   	@email = email
-  	attachments.inline['8bit-heart.png'] = File.read("#{Rails.root}/app/assets/images/emails/8bit-heart.png"
-  	attachments.inline['shadow.png'] = File.read("#{Rails.root}/app/assets/images/emails/shadow.png"
   	mail(to: email.getter_email, subject: "a love note from #{email.from}")
   end
 end
