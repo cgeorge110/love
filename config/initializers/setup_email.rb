fade7d2e948256149440bc0959a9303267e77481
Chris George <cgeorge110@gmail.com>
2014-02-09 19:05:46 -0800

    No Mas Email

--- config/initializers/setup_email.rb
+++
@@ -1,7 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-   :address => "email-smtp.us-west-2.amazonaws.com",
-    :user_name => ENV["SES_USERNAME"],
-    :password => ENV["SES_PASSWORD"],
-    :authentication => :login,
-    :enable_starttls_auto => true
-}
