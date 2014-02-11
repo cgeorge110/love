c88d26a5e1cafc3b3bcfc48f84c1b0f92cd3a7da
Chris George <cgeorge110@gmail.com>
2014-02-10 19:42:21 -0800

    Suck at punctuation

--- config/initializers/twitter_initializer.rb
+++ config/initializers/twitter_initializer.rb
@@ -4,9 +4,9 @@ require 'twitter'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 $client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "<%= ENV['TWITTER_CONSUMER_KEY'] %>"
-  config.consumer_secret     = "<%= ENV['TWITTER_CONSUMER_KEY'] %>"
-  config.access_token        = "<%= ENV['TWITTER_CONSUMER_KEY'] %>"
-  config.access_token_secret = "<%= ENV['TWITTER_CONSUMER_KEY'] %>"
+  config.consumer_key        = <%= ENV['TWITTER_CONSUMER_KEY'] %>
+  config.consumer_secret     = <%= ENV['TWITTER_CONSUMER_KEY'] %>
+  config.access_token        = <%= ENV['TWITTER_CONSUMER_KEY'] %>
+  config.access_token_secret = <%= ENV['TWITTER_CONSUMER_KEY'] %>
 end
 
