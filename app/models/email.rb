c4bda62f9ce6600f22ddd443ac357f33645ee384
Chris George <cgeorge110@gmail.com>
2014-02-10 21:48:46 -0800

    Add Validations

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,11 @@
 class Email < ActiveRecord::Base
 	belongs_to :note
+	validates :to, :presence => true
+	validates :getter_email,   
+            :presence => true,   
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
+    validates :from, :presence => true
+    validates :sender_email,   
+            :presence => true,   
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
 end
