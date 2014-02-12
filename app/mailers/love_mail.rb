831e1cb097e45d23b9079f4b5a944b1d8de4ccb3
Chris George <cgeorge110@gmail.com>
2014-02-11 22:16:44 -0800

    From Name

--- app/mailers/love_mail.rb
+++ app/mailers/love_mail.rb
@@ -1,7 +1,9 @@
 class LoveMail < ActionMailer::Base
+  default from: 'notes@somelovenotes.com'
+
   def love_note(email)
   	@email = email
   	from_with_name = '\"#{email.from} via SomeLoveNotes\" <notes@somelovenotes.com>'
-  	mail(to: email.getter_email, subject: "a love note from #{email.from}", from: from_with_name)
+  	mail(to: email.getter_email, subject: "a love note from #{email.from}")
   end
 end
