b234e8504085c5edd87b6d9ca39c411be07326b2
Chris George <cgeorge110@gmail.com>
2014-02-11 22:31:30 -0800

    Email Credentials

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,8 +2,10 @@ class Email < ActiveRecord::Base
 	belongs_to :note
 	validates :to, :presence => true
 	validates :getter_email,   
-            :presence => true
+            :presence => true,   
+            :email => true
     validates :from, :presence => true
     validates :sender_email,   
-            :presence => true
+            :presence => true,   
+            :email => true
 end
