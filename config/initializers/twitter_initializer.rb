3f65441866c62135a74e54c191445568b7c4a311
Chris George <cgeorge110@gmail.com>
2014-02-11 22:44:42 -0800

    Environment Changes

--- config/initializers/twitter_initializer.rb
+++ config/initializers/twitter_initializer.rb
@@ -5,8 +5,8 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 $client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-  config.consumer_secret     = ENV['TWITTER_CONSUMER_KEY']
-  config.access_token        = ENV['TWITTER_CONSUMER_KEY']
-  config.access_token_secret = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
