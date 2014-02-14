5bb75236cf1cc1ebfeb544b34b429e7365c3ab53
Chris George <cgeorge110@gmail.com>
2014-02-13 21:19:53 -0800

    set up for local dev

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Load local variables for development only
+aws_env = File.join(Rails.root, 'config', 'aws_env.rb')
+load(aws_env) if File.exists?(aws_env)
+
 # Initialize the Rails application.
 Lovesya::Application.initialize!
